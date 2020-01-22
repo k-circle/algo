80d3f74d582b3361c8ceedd33dd912871368a88e
JOSEPH KIM <josephkim.dev@gmail.com>
2020-01-21 21:12:27 -0600

    update fact fuction

--- py/week3_recursion.py
+++ py/week3_recursion.py
@@ -12,6 +12,14 @@ def fibonacci(n):
         return fibonacci(n-1)+fibonacci(n-2)
 
 
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
+
+
 if __name__ == '__main__':
     # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
     print(fibonacci(10))
+    print(fact(4))
