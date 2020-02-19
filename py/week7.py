28f138ed277b2265e717a7246730e54b3998615b
JOSEPH KIM <josephkim.dev@gmail.com>
2020-02-18 20:47:02 -0600

    Create week7.py

---
+++ py/week7.py
@@ -0,0 +1,53 @@
+def djikstra():
+    graph = {
+        'book': {
+            'lp': 5,
+            'poster': 0,
+        },
+        'lp': {
+            'bass': 15,
+            'drum': 20,
+        },
+        'poster': {
+            'bass': 20,
+            'drum': 35,
+        },
+        'bass': {
+            'piano': 20,
+        },
+        'drum': {
+            'piano': 10
+        },
+        'piano': {},
+    }
+    costs = {
+        'book': 0,
+        'lp': float('inf'),
+        'poster': float('inf'),
+        'bass': float('inf'),
+        'drum': float('inf'),
+        'piano': float('inf'),
+    }
+    visited = set()
+    path = {
+        'book': None,
+    }
+    while len(path) > len(visited):
+        node = min({k: v for k, v in costs.items() if k not in visited}, key=costs.get)  # min cost in not visited nodes
+
+        visited.add(node)
+        for k, v in graph[node].items():
+            if costs[k] > costs[node] + graph[node][k]:
+                costs[k] = costs[node] + graph[node][k]
+                path[k] = node
+    end = 'piano'
+    shortest_path = [end]
+    while path[end]:
+        shortest_path.insert(0, path[end])
+        end = path[end]
+    print('Path:', ' -> '.join(shortest_path))
+    print('Costs:', costs)
+
+
+if __name__ == '__main__':
+    djikstra()
